ab4b8ae1ab84a7cf8c5dfbf29f55a6901caff9aa
yzhang511 <yz4123@columbia.edu>
2024-02-27 23:15:32 -0500

    restructured datasets

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,8 +1,7 @@
 import numpy as np
 from scipy.sparse import csr_array
-from datasets import Dataset
+from datasets import Dataset, DatasetInfo
 from datasets import load_dataset
-
 
 def get_sparse_from_binned_spikes(binned_spikes):
     sparse_binned_spikes = [csr_array(binned_spikes[i], dtype=np.ubyte) for i in range(binned_spikes.shape[0])]
@@ -21,11 +20,7 @@ def get_binned_spikes_from_sparse(spikes_sparse_data_list, spikes_sparse_indices
 
     return binned_spikes
 
-def create_dataset(binned_spikes, bwm_df, chosen_idx, eid, probe, region_cluster_ids, beryl_reg, bin_size, binned_behaviors=None, metadata=None):
-    session_number = bwm_df.session_number[chosen_idx]
-    date = bwm_df.date[chosen_idx]
-    subject = bwm_df.subject[chosen_idx]
-    lab = bwm_df.lab[chosen_idx]
+def create_dataset(binned_spikes, bwm_df, eid, params, meta_data=None, binned_behaviors=None):
 
     # Scipy sparse matrices can't be directly loaded into HuggingFace Datasets so they are converted to lists
     sparse_binned_spikes, spikes_sparse_data_list, spikes_sparse_indices_list, spikes_sparse_indptr_list, spikes_sparse_shape_list = get_sparse_from_binned_spikes(binned_spikes)
@@ -35,24 +30,26 @@ def create_dataset(binned_spikes, bwm_df, chosen_idx, eid, probe, region_cluster
         'spikes_sparse_indices': spikes_sparse_indices_list,
         'spikes_sparse_indptr': spikes_sparse_indptr_list,
         'spikes_sparse_shape': spikes_sparse_shape_list,
-
-        'region_cluster_ids': [region_cluster_ids] * len(sparse_binned_spikes),
-        'brain_regions': [beryl_reg] * len(sparse_binned_spikes),
-        'bin_size': [bin_size] * len(sparse_binned_spikes),
-        'eid': [eid] * len(sparse_binned_spikes),
-        'probe_name': [probe] * len(sparse_binned_spikes),
-        'session_number': [session_number] * len(sparse_binned_spikes),
-        'date': [date] * len(sparse_binned_spikes),
-        'subject': [subject] * len(sparse_binned_spikes),
-        'lab': [lab] * len(sparse_binned_spikes),
     }
+    
     if binned_behaviors is not None:
-        # Store choice behaviors more efficiently
-        binned_behaviors["choice"] = np.where(binned_behaviors["choice"] > 0, 0, 1).astype(bool)
-        
+        # Store choice behaviors more efficiently (save this option for later)
+        # binned_behaviors["choice"] = np.where(binned_behaviors["choice"] > 0, 0, 1).astype(bool)
         data_dict = data_dict | binned_behaviors
-    if metadata is not None:
-        data_dict = data_dict | metadata.to_dict(orient='list')
+        
+    if meta_data is not None:
+        meta_dict = {
+            'binsize': [params['binsize']] * len(sparse_binned_spikes),
+            'interval_len': [params['interval_len']] * len(sparse_binned_spikes),
+            'eid': [meta_data['eid']] * len(sparse_binned_spikes),
+            'probe_name': [meta_data['probe_name']] * len(sparse_binned_spikes),
+            'subject': [meta_data['subject']] * len(sparse_binned_spikes),
+            'lab': [meta_data['lab']] * len(sparse_binned_spikes),
+            'cluster_regions': [meta_data['cluster_regions']] * len(sparse_binned_spikes),
+            'cluster_channels': [meta_data['cluster_channels']] * len(sparse_binned_spikes),
+            'good_clusters': [meta_data['good_clusters']] * len(sparse_binned_spikes)
+        }
+        data_dict = data_dict | meta_dict
 
     return Dataset.from_dict(data_dict)
 
