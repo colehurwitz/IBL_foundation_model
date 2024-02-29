44cdca3cd7232c6ef04663897480ae04f9763ebb
yzhang511 <yz4123@columbia.edu>
2024-02-28 23:26:38 -0500

    parallel + behavior loss + add sample freq

--- utils/dataset.py
+++ utils/dataset.py
@@ -45,6 +45,7 @@ def create_dataset(binned_spikes, bwm_df, eid, params, meta_data=None, binned_be
             'probe_name': [meta_data['probe_name']] * len(sparse_binned_spikes),
             'subject': [meta_data['subject']] * len(sparse_binned_spikes),
             'lab': [meta_data['lab']] * len(sparse_binned_spikes),
+            'sampling_freq': [meta_data['sampling_freq']] * len(sparse_binned_spikes),
             'cluster_regions': [meta_data['cluster_regions']] * len(sparse_binned_spikes),
             'cluster_channels': [meta_data['cluster_channels']] * len(sparse_binned_spikes),
             'good_clusters': [meta_data['good_clusters']] * len(sparse_binned_spikes)
