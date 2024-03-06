e0c494eb68068efbbc875c7fccd1fa5ec2e56fae
PPWangyc <ppwangyc@gmail.com>
2024-03-06 03:31:00 -0500

    add test, wandb

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,6 +2,7 @@ import os
 import random
 import numpy as np
 import torch
+import matplotlib.pyplot as plt
 
 def set_seed(seed):
     # set seed for reproducibility
@@ -20,4 +21,18 @@ def move_batch_to_device(batch, device):
         if isinstance(batch[key], torch.Tensor):
             batch[key] = batch[key].to(device)
     return batch
+
+def plot_gt_pred(gt, pred, epoch=0):
+    # plot Ground Truth and Prediction in the same figur
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+    ax1.set_title("Ground Truth")
+    im1 = ax1.imshow(gt, aspect='auto', cmap='binary')
+    
+    ax2.set_title("Prediction")
+    im2 = ax2.imshow(pred, aspect='auto', cmap='binary')
+    # color adjust for all subplots
+    plt.colorbar(im1, ax=[ax1, ax2])
+
+    fig.suptitle("Epoch: {}".format(epoch))
+    return fig
     
