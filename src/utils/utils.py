1d86a348719bf65aad32310d3a8af1b293552a36
PPWangyc <ppwang@stanford.edu>
2024-03-07 03:44:17 -0500

    add r2 metric and mse loss

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+from utils.metric_utils import r2_score
 
 def set_seed(seed):
     # set seed for reproducibility
@@ -30,9 +31,25 @@ def plot_gt_pred(gt, pred, epoch=0):
     
     ax2.set_title("Prediction")
     im2 = ax2.imshow(pred, aspect='auto', cmap='binary')
-    # color adjust for all subplots
-    plt.colorbar(im1, ax=[ax1, ax2])
+    
+    # add colorbar
+    fig.colorbar(im1, ax=ax1)
+    fig.colorbar(im2, ax=ax2)
 
     fig.suptitle("Epoch: {}".format(epoch))
     return fig
+
+# metrics list, return different metrics results
+def metrics_list(gt, pred, metrics=["r2", "mse", "mae"], device="cpu"):
+    results = {}
+    if "r2" in metrics:
+        r2 = r2_score(gt, pred)
+        results["r2"] = r2
+    if "mse" in metrics:
+        mse = torch.mean((gt - pred) ** 2)
+        results["mse"] = mse
+    if "mae" in metrics:
+        mae = torch.mean(torch.abs(gt - pred))
+        results["mae"] = mae
+    return results
     
