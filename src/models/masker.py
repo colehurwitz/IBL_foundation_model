bf4735ab3b8d54cf5e69b99f12666f1ee5140038
donatojb <donatojbeneto@gmail.com>
2024-03-27 12:20:19 -0400

    Masker in NDT1

--- src/models/masker.py
+++ src/models/masker.py
@@ -1,9 +1,9 @@
-from typing import Tuple
+from typing import Tuple, Optional
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import numpy as np
 from utils.config_utils import DictConfig
 
 
@@ -20,7 +20,7 @@ CONFIG:
     random_ratio: of the not zeroed out, fraction of randomly set. the rest are left unchanged
     expand_prob: probability of expanding the mask for consecutive bin masking in ``temporal`` mode
     max_timespan: max length of the expanded mask in ``temporal`` mode
-    mask_regions: list of ``str`` containing the names of regions to mask in ``region`` mode
+    regions: list of ``str`` containing the names of regions to mask in ``region`` mode
 """
 class Masker(nn.Module):
 
@@ -28,36 +28,40 @@ class Masker(nn.Module):
     def __init__(self, config: DictConfig):
         super().__init__()
 
-        self.mode = config.mode             
+        self.mode = config.mode          
         self.ratio = config.ratio
         self.zero_ratio = config.zero_ratio
         self.random_ratio = config.random_ratio
         self.expand_prob = config.expand_prob
         self.max_timespan = config.max_timespan
-        self.mask_regions = config.mask_indx
+        self.regions = config.regions
 
     def forward(
         self, 
         spikes: torch.FloatTensor,                      # (bs, seq_len, n_channels)
-        regions: List[List[str]],                       # (bs, n_channels)     
+        brain_regions: Optional[np.ndarray] = None,     # (bs, n_channels)     
     ) -> Tuple[torch.FloatTensor,torch.LongTensor]:     # (bs, seq_len, n_channels), (bs, seq_len, n_channels)
 
         mask_ratio = self.ratio
-        # Expand mask
-        if self.mode == "timestep":
+        if self.mode == "temporal":
+            # Expand mask
             if torch.bernoulli(torch.tensor(self.expand_prob).float()):
                 timespan = torch.randint(1, self.max_timespan+1, (1, )).item() 
             else:
                 timespan = 1
             mask_ratio = mask_ratio/timespan
-
-        # Get masking probabilities
-        if self.mode == "full":
-            mask_probs = torch.full(spikes.shape, mask_ratio)     # (bs, seq_len, n_channels)
-        elif self.mode == "timestep":
             mask_probs = torch.full(spikes[:, :, 0].shape, mask_ratio) # (bs, seq_len)
         elif self.mode == "neuron":
             mask_probs = torch.full(spikes[:, 0].shape, mask_ratio)    # (bs, n_channels)
+        elif self.mode == "random":
+            mask_probs = torch.full(spikes.shape, mask_ratio)     # (bs, seq_len, n_channels)
+        elif self.mode == "region":
+            assert brain_regions is not None, "Can't mask region without brain region information"
+            region_indx = []
+            mask_probs = torch.zeros(spikes.shape[0],spikes.shape[2])
+            for region in self.regions:
+                region_indx = torch.tensor(brain_regions == region, device=spikes.device)
+                mask_probs[region_indx] = 1
         else:
             raise Exception(f"Masking mode {self.mode} not implemented")
         
@@ -65,13 +69,14 @@ class Masker(nn.Module):
         mask = torch.bernoulli(mask_probs).to(spikes.device)
 
         # Expand mask
-        if self.mode == "timestep":
+        if self.mode == "temporal":
             mask = self.expand_timesteps(mask, timespan)
             mask = mask.unsqueeze(2).expand_as(spikes).bool()    # (bs, seq_len, n_channels)
-        elif self.mode == "neuron":
+        elif self.mode in ["neuron","region"]:
             mask = mask.unsqueeze(1).expand_as(spikes).bool()    # (bs, seq_len, n_channels)
-        else: # full
+        else: # random
             mask = mask.bool()
+            
         # Mask data
         zero_idx = torch.bernoulli(torch.full(spikes.shape, self.zero_ratio)).to(spikes.device).bool() & mask
         spikes[zero_idx] = 0
@@ -86,3 +91,5 @@ class Masker(nn.Module):
         kernel = torch.ones(width, device=mask.device).view(1, 1, -1)
         expanded_mask = F.conv1d(mask.unsqueeze(1), kernel, padding="same")
         return (expanded_mask.squeeze(1) >= 1)
+
+
