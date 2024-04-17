6d7f1ad66358298ac1acbf1d05596df92b6e7f83
PPWangyc <ppwang@stanford.edu>
2024-04-17 03:45:00 +0000

    add val and single session

--- src/eval_model.py
+++ src/eval_model.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset, load_from_disk, concatenate_datasets
+from utils.dataset_utils import load_ibl_dataset
 from accelerate import Accelerator
 import torch
 import numpy as np
@@ -6,9 +7,13 @@ import os
 import matplotlib.pyplot as plt
 
 from loader.make_loader import make_loader
-from utils.utils import set_seed,move_batch_to_device, viz_single_cell
+from utils.utils import (set_seed,move_batch_to_device, 
+                         viz_single_cell, 
+                         prep_cond_matrix,
+                         var_name2idx,
+                         var_value2label,
+                         var_tasklist)
 from utils.config_utils import config_from_kwargs, update_config
-from utils.dataset_utils import get_data_from_h5
 from models.ndt1 import NDT1
 from models.stpatch import STPatch
 
@@ -16,17 +21,20 @@ from models.stpatch import STPatch
 
 # load config
 kwargs = {
-    "model": "include:src/configs/co_smooth_ndt1.yaml"
+    "model": "include:src/configs/ndt2.yaml"
 }
 
 config = config_from_kwargs(kwargs)
-config = update_config("src/configs/co_smooth_ndt1.yaml", config)
-config = update_config("src/configs/trainer.yaml", config)
+config = update_config("src/configs/ndt2.yaml", config)
+config = update_config("src/configs/ssl_session_trainer.yaml", config)
 
 mask_mode = config.encoder.masker.mode
 
 # make log dir
-log_dir = os.path.join(config.dirs.log_dir, "eval", "model_{}".format(config.model.model_class), "method_{}".format(config.method.model_kwargs.method_name), "mask_{}".format(mask_mode))
+log_dir = os.path.join(config.dirs.log_dir, 
+                       "eval", "model_{}".format(config.model.model_class), 
+                       "method_{}".format(config.method.model_kwargs.method_name), 
+                       "mask_{}".format(mask_mode))
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
@@ -38,54 +46,22 @@ if config.wandb.use:
 # set seed for reproducibility
 set_seed(config.seed)
 
+eid = "671c7ea7-6726-4fbe-adeb-f89c2c8e489b"
 # download dataset from huggingface
-if "ibl" in config.data.dataset_name:
-    dataset = load_dataset(config.dirs.dataset_dir, cache_dir=config.dirs.dataset_cache_dir)
-    # show the columns
-    
-    bin_size = dataset["train"]["bin_size"][0]
-    
-
-    # split the dataset to train and test
-    dataset = dataset["train"].train_test_split(test_size=0.1, seed=config.seed)
-    # select the train dataset and the spikes_sparse_data column
-    data_columns = ['spikes_sparse_data', 'spikes_sparse_indices', 'spikes_sparse_indptr', 'spikes_sparse_shape']
-    train_dataset = dataset["train"].select_columns(data_columns)
-    test_dataset = dataset["test"].select_columns(data_columns)
-
-    if config.data.include_behav:
-        dataset = load_from_disk(os.path.join('data', config.dirs.behav_dir))
-        dataset = concatenate_datasets([dataset["train"], dataset["val"], dataset["test"]])
-        dataset = dataset.train_test_split(test_size=0.1, seed=config.seed)
-        bin_size = dataset["train"]["binsize"][0]
-
-        train_dataset = dataset["train"]
-        test_dataset = dataset["test"]
-
-    print(dataset.column_names)
-    print(f"bin_size: {bin_size}")
-
-else:
-    train_dataset = get_data_from_h5("train", config.dirs.dataset_dir, config=config)
-    test_dataset = get_data_from_h5("val", config.dirs.dataset_dir, config=config)
-    bin_size = None
+_, _, test_dataset = load_ibl_dataset(config.dirs.dataset_cache_dir, 
+                           config.dirs.huggingface_org,
+                           aligned_data_dir=config.dirs.aligned_data_dir,
+                           eid=eid,
+                           num_sessions=config.data.num_sessions,
+                           split_method=config.data.split_method,
+                           test_session_eid=config.data.test_session_eid,
+                           seed=config.seed)
 
 # make the dataloader
-train_dataloader = make_loader(train_dataset, 
-                         batch_size=config.training.train_batch_size, 
-                         pad_to_right=True, 
-                         pad_value=-1.,
-                         bin_size=bin_size,
-                         max_time_length=config.data.max_time_length,
-                         max_space_length=config.data.max_space_length,
-                         dataset_name=config.data.dataset_name,
-                         shuffle=True)
-
 test_dataloader = make_loader(test_dataset, 
                          batch_size=config.training.test_batch_size, 
                          pad_to_right=True, 
                          pad_value=-1.,
-                         bin_size=bin_size,
                          max_time_length=config.data.max_time_length,
                          max_space_length=config.data.max_space_length,
                          dataset_name=config.data.dataset_name,
@@ -109,15 +85,23 @@ preds = []
 with torch.no_grad():
     for batch in test_dataloader:
         batch = move_batch_to_device(batch, accelerator.device)
-        if config.data.patching:
-            gt.append(outputs.targets.clone())
-        else:
+        if not config.data.patching:
             gt.append(batch['spikes_data'].clone())
-        outputs = model(
+            outputs = model(
+                batch['spikes_data'], 
+                batch['attention_mask'], 
+                batch['spikes_timestamps']
+            )
+        else:
+            outputs = model(
             batch['spikes_data'], 
-            batch['attention_mask'], 
-            batch['spikes_timestamps']
+            batch['time_attn_mask'],
+            batch['space_attn_mask'],
+            batch['spikes_timestamps'], 
+            batch['spikes_spacestamps'], 
+            targets = batch['target']
         )
+            gt.append(outputs.targets.clone())
         preds.append(outputs.preds.clone())
 
 gt = torch.cat(gt, dim=0)
@@ -126,51 +110,12 @@ preds = torch.cat(preds, dim=0)
 if config.method.model_kwargs.loss == "poisson_nll":
     preds = torch.exp(preds)
 
-# prepare the condition matrix
-b_list = []
-
-# choice
-choice = np.array(test_dataset['choice'])
-choice = np.tile(np.reshape(choice, (choice.shape[0], 1)), (1, 100))
-b_list.append(choice)
-
-# reward
-reward = np.array(test_dataset['reward'])
-reward = np.tile(np.reshape(reward, (reward.shape[0], 1)), (1, 100))
-b_list.append(reward)
-
-# block
-block = np.array(test_dataset['block'])
-block = np.tile(np.reshape(block, (block.shape[0], 1)), (1, 100))
-b_list.append(block)
-
-# wheel
-wheel = np.array(test_dataset['wheel-speed'])
-b_list.append(wheel)
-
-behavior_set = np.stack(b_list,axis=-1)
+behavior_set = prep_cond_matrix(test_dataset)
 
 # Settings for validation
 X = behavior_set # [#trials, #timesteps, #variables]
 ys = gt.cpu().numpy() # [#trials, #timesteps, #neurons]
 y_preds = preds.cpu().numpy() # [#trials, #timesteps, #neurons]
-
-var_name2idx = {'block':[2], 
-                'choice': [0], 
-                'reward': [1], 
-                'wheel': [3],
-                }
-
-var_value2label = {'block': {(0.2,): "p(left)=0.2",
-                            (0.5,): "p(left)=0.5",
-                            (0.8,): "p(left)=0.8",},
-                   'choice': {(-1.0,): "right",
-                            (1.0,): "left"},
-                   'reward': {(0.,): "no reward",
-                            (1.,): "reward", } }
-
-var_tasklist = ['block','choice','reward']
-var_behlist = []
 
 # choose more active neuron
 tmp = np.mean(ys, axis=(0,1))
@@ -179,12 +124,27 @@ if mask_mode == "co-smooth":
     idx_top = config.encoder.masker.channels
     idx_top = np.array(idx_top)
 
+var_behlist = []
+
+psth_r2_list = []
+pred_r2_list = []
 for i in range(idx_top.shape[0]):
-    viz_single_cell(X,ys[:,:,idx_top[i]],y_preds[:,:,idx_top[i]], 
+    metrics = viz_single_cell(X,ys[:,:,idx_top[i]],y_preds[:,:,idx_top[i]], 
                     var_name2idx, var_tasklist, var_value2label, var_behlist,
                     subtract_psth="task", aligned_tbins=[26], neuron_idx=idx_top[i])
+    
+    psth_r2_list.append(metrics['psth_r2'])
+    pred_r2_list.append(metrics['pred_r2'])
     plt.savefig(os.path.join(log_dir, f"neuron_{idx_top[i]}.png"))
     # wandb
     if config.wandb.use:
         wandb.log({f"neuron_{idx_top[i]}": wandb.Image(os.path.join(log_dir, f"neuron_{idx_top[i]}.png"))})
+
+print("Mean PSTH R2: {}, STD PSTH R2: {}".format(np.mean(psth_r2_list), np.std(psth_r2_list)))
+print("Mean Pred R2: {}, STD Pred R2: {}".format(np.mean(pred_r2_list), np.std(pred_r2_list)))
+if config.wandb.use:
+    wandb.log({"mean_psth_r2": np.mean(psth_r2_list), 
+               "std_psth_r2": np.std(psth_r2_list),
+               "mean_pred_r2": np.mean(pred_r2_list),
+                "std_pred_r2": np.std(pred_r2_list)})
     
--- src/train_session.py
+++ src/train_session.py
@@ -21,44 +21,39 @@ kwargs = {
 
 config = config_from_kwargs(kwargs)
 config = update_config("src/configs/ndt2.yaml", config)
-config = update_config("src/configs/trainer.yaml", config)
+config = update_config("src/configs/ssl_session_trainer.yaml", config)
 
 # make log dir
-log_dir = os.path.join(config.dirs.log_dir, "train", "model_{}".format(config.model.model_class), "method_{}".format(config.method.model_kwargs.method_name), "mask_{}".format(config.encoder.masker.mode))
+log_dir = os.path.join(config.dirs.log_dir, 
+                       "train", 
+                       "single",
+                       "model_{}".format(config.model.model_class), 
+                       "method_{}".format(config.method.model_kwargs.method_name), 
+                       "mask_{}".format(config.encoder.masker.mode))
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
 # wandb
 if config.wandb.use:
     import wandb
-    wandb.init(project=config.wandb.project, entity=config.wandb.entity, config=config, name="train_model_{}_method_{}_mask_{}".format(config.model.model_class, config.method.model_kwargs.method_name,config.encoder.masker.mode))
+    wandb.init(project=config.wandb.project, 
+               entity=config.wandb.entity, 
+               config=config, 
+               name="train_model_{}_method_{}_mask_{}".format(config.model.model_class, config.method.model_kwargs.method_name,config.encoder.masker.mode))
 
 # set seed for reproducibility
 set_seed(config.seed)
 
+eid = "671c7ea7-6726-4fbe-adeb-f89c2c8e489b"
 # download dataset from huggingface
-eid = None
-train_dataset, test_dataset = load_ibl_dataset(config.dirs.dataset_cache_dir, 
+train_dataset, val_dataset, test_dataset = load_ibl_dataset(config.dirs.dataset_cache_dir, 
                            config.dirs.huggingface_org,
+                           aligned_data_dir=config.dirs.aligned_data_dir,
                            eid=eid,
                            num_sessions=config.data.num_sessions,
                            split_method=config.data.split_method,
                            test_session_eid=config.data.test_session_eid,
                            seed=config.seed)
-if config.data.use_aligned_test:
-    # aligned dataset
-    if eid is None:
-        test_dataset = load_from_disk(os.path.join('data', config.dirs.behav_dir))
-        data_columns = ['spikes_sparse_data', 'spikes_sparse_indices', 'spikes_sparse_indptr', 'spikes_sparse_shape']
-        test_dataset = concatenate_datasets([test_dataset["train"], test_dataset["val"], test_dataset["test"]])
-        test_dataset = test_dataset.select_columns(data_columns)
-    else:
-        aligned_dataset = load_from_disk(os.path.join('data', config.dirs.behav_dir))
-        aligned_dataset = concatenate_datasets([aligned_dataset["train"], aligned_dataset["val"], aligned_dataset["test"]])
-        train_dataset, test_dataset = split_both_dataset(aligned_dataset=aligned_dataset,
-                                                         unaligned_dataset=train_dataset,
-                                                         seed=config.seed)
-
 
 # make the dataloader
 train_dataloader = make_loader(train_dataset, 
@@ -71,7 +66,7 @@ train_dataloader = make_loader(train_dataset,
                          dataset_name=config.data.dataset_name,
                          shuffle=True)
 
-test_dataloader = make_loader(test_dataset, 
+eval_dataloader = make_loader(val_dataset, 
                          target=config.data.target,
                          batch_size=config.training.test_batch_size, 
                          pad_to_right=True, 
@@ -90,7 +85,10 @@ model_class = NAME2MODEL[config.model.model_class]
 model = model_class(config.model, **config.method.model_kwargs)
 model = accelerator.prepare(model)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=config.optimizer.lr, weight_decay=config.optimizer.wd, eps=config.optimizer.eps)
+optimizer = torch.optim.AdamW(model.parameters(), 
+                              lr=config.optimizer.lr, 
+                              weight_decay=config.optimizer.wd, 
+                              eps=config.optimizer.eps)
 lr_scheduler = OneCycleLR(
                 optimizer=optimizer,
                 total_steps=config.training.num_epochs*len(train_dataloader) //config.optimizer.gradient_accumulation_steps,
@@ -108,7 +106,7 @@ trainer_kwargs = {
 trainer = make_trainer(
     model=model,
     train_dataloader=train_dataloader,
-    eval_dataloader=test_dataloader,
+    eval_dataloader=eval_dataloader,
     optimizer=optimizer,
     **trainer_kwargs
 )
