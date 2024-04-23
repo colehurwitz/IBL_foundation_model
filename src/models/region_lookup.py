8af8c11bc03fedd0a406d3fc4317708a00e46c0b
yzhang511 <yz4123@columbia.edu>
2024-04-23 14:15:41 -0400

    init region lookup (unfinished)

---
+++ src/models/region_lookup.py
@@ -0,0 +1,15 @@
+from iblatlas.regions import BrainRegions
+
+"""
+Work in progress. Create a global lookup table for all brain regions.
+"""
+
+class RegionLookup(object):
+    def __init__(self):
+        self.brain_regions = BrainRegions().acronym
+        self.region_to_indx = {r: i for i,r in enumerate(self.brain_regions)}
+        self.indx_to_region = {v: k for k,v in self.region_to_indx.items()}
+
+    def lookup(regions):
+        pass
+            
