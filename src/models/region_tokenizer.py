23a4b65ec98435bfa4c023fb1f6e7b827df16835
yzhang511 <yz4123@columbia.edu>
2024-04-23 14:15:41 -0400

    init region lookup (unfinished)

---
+++ src/models/region_tokenizer.py
@@ -0,0 +1,15 @@
+from iblatlas.regions import BrainRegions
+
+"""
+Work in progress. Create a global lookup table for all brain regions.
+"""
+
+class RegionTokenizer(object):
+    def __init__(self):
+        self.brain_regions = BrainRegions().acronym
+        self.region_to_indx = {r: i for i,r in enumerate(self.brain_regions)}
+        self.indx_to_region = {v: k for k,v in self.region_to_indx.items()}
+
+    def tokenize(regions):
+        pass
+            
