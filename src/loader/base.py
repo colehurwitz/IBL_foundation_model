fix random token masking for ndt2

src/loader/base.py
@@ -14,8 +14,8 @@ def _pad_seq_right_to_n(
             seq,
             np.ones(
                 (
-                    n-seq.shape[0],
-                    *seq.shape[1:]
+                    n-len(seq),
+                    *seq[0].shape
                 )
             ) * pad_value,  
         ],
@@ -33,8 +33,8 @@ def _pad_seq_left_to_n(
         [
             np.ones(
                 (
-                    n-seq.shape[0],
-                    *seq.shape[1:]
+                    n-len(seq),
+                    *seq[0].shape
                 )
             ) * pad_value,
             seq,
@@ -230,7 +230,7 @@ class BaseDataset(torch.utils.data.Dataset):
             "spikes_spacestamps": spikes_spacestamps,
             "target": target_behavior,
             "neuron_depths": neuron_depths, 
-            "neuron_regions": neuron_regions
+            "neuron_regions": list(neuron_regions)
         }
     
     def __len__(self):

src/models/masker.py
@@ -60,7 +60,7 @@ class Masker(nn.Module):
             return spikes, torch.zeros_like(spikes)
 
         mask_ratio = self.ratio
-        if self.mode == "temporal":
+        if self.mode in ["temporal", "random_token"]:
             # Expand mask
             if torch.bernoulli(torch.tensor(self.expand_prob).float()):
                 timespan = torch.randint(1, self.max_timespan+1, (1, )).item() 
@@ -108,7 +108,7 @@ class Masker(nn.Module):
         mask = torch.bernoulli(mask_probs).to(spikes.device)
 
         # Expand mask
-        if self.mode == "temporal":
+        if self.mode in ["temporal", "random_token"]:
             if timespan > 1:
                 mask = self.expand_timesteps(mask, timespan)
             mask = mask.unsqueeze(2).expand_as(spikes).bool()    