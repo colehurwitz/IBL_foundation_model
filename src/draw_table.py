bb08f858f41c3032fabf8ec0bdbe000e0aa40b11
Yizi Zhang <yzhang1@pcn-7-09.cm.cluster>
2024-05-03 11:31:22 -0700

    final fix for train eval

--- src/draw_table.py
+++ src/draw_table.py
@@ -2,66 +2,73 @@ from pathlib import Path
 import numpy as np
 from matplotlib import pyplot as plt
 
-model = "NDT2"
+model = "NDT1"
 
-mask_methods = ['mask_neuron', 'mask_causal', 'mask_intra-region', 'mask_inter-region', 'mask_temporal']
-eval_methods = ['co_smooth', 'forward_pred', 'intra_region', 'inter_region', 'choice_decoding', 'continuous_decoding']
+mask_ratio = 0.3
+mask_methods = ['mask_neuron', 'mask_causal', 'mask_intra-region', 'mask_inter-region', 'mask_temporal', 'mask_combined', 'mask_all']
+eval_methods = ['co_smooth', 'forward_pred', 'intra_region', 'inter_region']
+finetune_methods = ['choice_decoding', 'continuous_decoding']
 
 if model == "NDT2":
     mask_methods += ['mask_random_token']
 
-save_path = Path(f'/home/yizi/IBL_foundation_model/figs/model_{model}/method_ssl')
+save_path = Path(f'/mnt/home/yzhang1/ceph/results/eval/model_{model}/method_ssl')
 
 metrics_dict = {}
 for mask in mask_methods:
     metrics_dict[mask] = {}
+    if mask == 'mask_inter-region':
+        ratio = 0.0
+    else:
+        ratio = mask_ratio
     for eval in eval_methods:
         metrics_dict[mask][eval] = {}
-        if eval == "choice_decoding":
+        try:
+            r2 = np.load(save_path/mask/f'ratio_{ratio}'/eval/'r2.npy')
+        except:
+            r2 = np.zeros(2)
+        try:
+            bps = np.load(save_path/mask/f'ratio_{ratio}'/eval/'bps.npy')
+        except:
+            bps = 0
+        metrics_dict[mask][eval]['r2_psth'] = np.nanmean(r2.T[0]) 
+        metrics_dict[mask][eval]['r2_per_trial'] = np.nanmean(r2.T[1]) 
+        metrics_dict[mask][eval]['bps'] = np.nanmean(bps) 
+    for eval in finetune_methods:
+        metrics_dict[mask][eval] = {}
+        if eval == "choice_decoding":                
             try:
-                acc = np.load(save_path/mask/eval/'choice_results.npy', allow_pickle=True).item()['acc']
+                acc = np.load(save_path/mask/f'ratio_{ratio}'/eval/'choice_results.npy', allow_pickle=True).item()['acc']
             except:
                 acc = np.zeros(1)
-            metrics_dict[mask][eval]['r2_psth'] = acc
-            metrics_dict[mask][eval]['r2_per_trial'] = acc
-            metrics_dict[mask][eval]['bps'] = acc
+            metrics_dict[mask][eval]['metric'] = acc
         elif eval == "continuous_decoding":
             try:
                 r2 = np.load(
-                    save_path/mask/eval/'left-whisker-motion-energy_results.npy', allow_pickle=True
+                    save_path/mask/f'ratio_{ratio}'/eval/'left-whisker-motion-energy_results.npy', allow_pickle=True
                 ).item()['r2']
             except:
                 r2 = np.zeros(1)
-            metrics_dict[mask][eval]['r2_psth'] = r2
-            metrics_dict[mask][eval]['r2_per_trial'] = r2
-            metrics_dict[mask][eval]['bps'] = r2
-        else:
-            try:
-                r2 = np.load(save_path/mask/eval/'r2.npy')
-            except:
-                r2 = np.zeros(2)
-            try:
-                bps = np.load(save_path/mask/eval/'bps.npy')
-            except:
-                bps = 0
-            metrics_dict[mask][eval]['r2_psth'] = np.nanmean(r2.T[0]) if np.nanmean(r2.T[0]) > -10 else -5
-            metrics_dict[mask][eval]['r2_per_trial'] = np.nanmean(r2.T[1]) if np.nanmean(r2.T[1]) > -10 else -5
-            metrics_dict[mask][eval]['bps'] = np.nanmean(bps) if np.nanmean(bps) > -10 else -5
+            metrics_dict[mask][eval]['metric'] = r2
 
 N = len(mask_methods)
 K = len(eval_methods)
+M = len(finetune_methods)
 r2_psth_mat, r2_per_trial_mat, bps_mat = np.zeros((N, K)), np.zeros((N, K)), np.zeros((N, K))
+behave_mat = np.zeros((N, M))
 for i, mask in enumerate(mask_methods):
     for j, eval in enumerate(eval_methods):
         r2_psth_mat[i,j] = metrics_dict[mask][eval]['r2_psth']
         r2_per_trial_mat[i,j] = metrics_dict[mask][eval]['r2_per_trial']
         bps_mat[i,j] = metrics_dict[mask][eval]['bps']
+    for j, eval in enumerate(finetune_methods):
+        behave_mat[i,j] = metrics_dict[mask][eval]['metric']
 
-fig, axes = plt.subplots(1,3, figsize=(12,4))
+fig, axes = plt.subplots(1, 4, figsize=(17.5, 5))
 
 mat = bps_mat
 im0 = axes[0].imshow(mat, cmap='RdYlGn')
-axes[0].set_title("bps")
+axes[0].set_title("co-bps")
 
 for i in range(len(mask_methods)):
     for j in range(len(eval_methods)):
@@ -71,7 +78,7 @@ for i in range(len(mask_methods)):
 
 mat = r2_psth_mat
 im1 = axes[1].imshow(mat, cmap='RdYlGn')
-axes[1].set_title("r2 psth")
+axes[1].set_title(r"$R^2$ PSTH")
 
 for i in range(len(mask_methods)):
     for j in range(len(eval_methods)):
@@ -81,7 +88,7 @@ for i in range(len(mask_methods)):
 
 mat = r2_per_trial_mat
 im2 = axes[2].imshow(mat, cmap='RdYlGn')
-axes[2].set_title("r2 per trial")
+axes[2].set_title(r"$R^2$ per trial")
 
 for i in range(len(mask_methods)):
     for j in range(len(eval_methods)):
@@ -89,15 +96,28 @@ for i in range(len(mask_methods)):
         text = axes[2].text(j, i, f'{mat[i, j]:.2f}',
                        ha="center", va="center", color=color, fontsize=12)
 
-for ax in axes:
+mat = behave_mat
+im2 = axes[3].imshow(mat, cmap='RdYlGn')
+axes[3].set_title(r"behavior decoding")
+
+for i in range(len(mask_methods)):
+    for j in range(len(finetune_methods)):
+        color = 'w' if mat[i, j] < 0.5 else 'k'
+        text = axes[3].text(j, i, f'{mat[i, j]:.2f}',
+                       ha="center", va="center", color=color, fontsize=12)
+
+for i, ax in enumerate(axes):
     if model == "NDT2":
-        ax.set_yticks(np.arange(N), labels=['neuron mask','causal mask', 'intra-region mask', 'inter-region mask', 'temporal mask', 'random token mask'])
+        ax.set_yticks(np.arange(N), labels=['neuron mask','causal mask', 'intra-region mask', 'inter-region mask', 'temporal mask', 'neuron+temporal+causal mask', 'all mask', 'random token mask'])
     else:
-        ax.set_yticks(np.arange(N), labels=['neuron mask','causal mask', 'intra-region mask', 'inter-region mask', 'temporal mask'])
-    ax.set_xticks(np.arange(K), labels=['co-smooth','forward pred', 'intra-region', 'inter-region', 'choice decoding', 'continuous decoding'])
+        ax.set_yticks(np.arange(N), labels=['neuron mask','causal mask', 'intra-region mask', 'inter-region mask', 'temporal mask', 'neuron+temporal+causal mask', 'all mask'])
+    if i < len(axes)-1:
+        ax.set_xticks(np.arange(K), labels=['co-smooth','forward pred', 'intra-region', 'inter-region'])
+    else:
+        ax.set_xticks(np.arange(M), labels=['choice', 'whisker motion energy'])
     
     plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
              rotation_mode="anchor")
 
 fig.tight_layout()
-plt.savefig(f'figs/table/{model}_metrics.png')
+plt.savefig(f'results/table/{model}_metrics.png')
