0d925b8fd777b8b674a70f97c7756fc00d1a2cf3
Yizi Zhang <yzhang39@exp-1-03.expanse.sdsc.edu>
2024-05-13 11:25:30 -0700

    fix

--- src/utils/eval_utils.py
+++ src/utils/eval_utils.py
@@ -1291,7 +1291,7 @@ def viz_single_cell(X, y, y_pred, var_name2idx, var_tasklist, var_value2label, v
 
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    plt.savefig(os.path.join(save_path, f'{neuron_region.replace('/', '-')}_{neuron_idx}_{r2_trial:.2f}_{method}.png'))
+    plt.savefig(os.path.join(save_path, f"{neuron_region.replace('/', '-')}_{neuron_idx}_{r2_trial:.2f}_{method}.png"))
     plt.tight_layout();
     # plt.show()
 
@@ -1355,7 +1355,7 @@ def viz_single_cell_unaligned(
     
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    plt.savefig(os.path.join(save_path, f'{neuron_region.replace('/', '-')}_{neuron_idx}_{r2:.2f}_{method}.png'))
+    plt.savefig(os.path.join(save_path, f"{neuron_region.replace('/', '-')}_{neuron_idx}_{r2:.2f}_{method}.png"))
     plt.tight_layout();
     return r2
 
