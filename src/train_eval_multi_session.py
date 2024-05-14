b47dc68f7ed4e806d72f56dee27927c8166f6c0f
Yizi Zhang <yzhang1@pcn-7-09.cm.cluster>
2024-05-13 23:07:37 -0700

    multi-session training

--- src/train_eval_multi_session.py
+++ src/train_eval_multi_session.py
@@ -24,12 +24,13 @@ warnings.simplefilter("ignore")
 base_path = '/mnt/home/yzhang1/ceph'
 
 ap = argparse.ArgumentParser()
-ap.add_argument("--test_eid", type=str)
+ap.add_argument("--test_eid", type=str, default='51e53aff-1d5d-4182-a684-aba783d50ae5')
 ap.add_argument("--mask_ratio", type=float, default=0.3)
 ap.add_argument("--mask_mode", type=str, default="all")
 ap.add_argument("--model_name", type=str, default="NDT1")
 ap.add_argument("--prompting", action='store_true')
 ap.add_argument("--train", action='store_true')
+ap.add_argument("--eval", action='store_true')
 args = ap.parse_args()
 
 eid = args.test_eid
@@ -48,9 +49,11 @@ else:
 config = config_from_kwargs(kwargs)
 config = update_config("src/configs/ssl_sessions_trainer.yaml", config)
 
+num_train_sessions = len(config.data.train_session_eid)
+
 if config.wandb.use:
     import wandb
-    wandb.init(project=config.wandb.project, entity=config.wandb.entity, config=config, name="train_multi_session_model_{}_method_{}_mask_{}_stitch_{}".format(config.model.model_class, config.method.model_kwargs.method_name,config.model.encoder.masker.mode, config.model.encoder.stitching))
+    wandb.init(project=config.wandb.project, entity=config.wandb.entity, config=config, name="train_multi_session_model_{}_method_{}_mask_{}_stitch_{}_{}_sessions".format(config.model.model_class, config.method.model_kwargs.method_name,config.model.encoder.masker.mode, config.model.encoder.stitching, num_train_sessions))
 
 set_seed(config.seed)
 
@@ -64,7 +67,9 @@ if args.train:
                            "model_{}".format(config.model.model_class), 
                            "method_{}".format(config.method.model_kwargs.method_name), 
                            "mask_{}".format(config.model.encoder.masker.mode),
-                           "stitch_{}".format(config.model.encoder.stitching))
+                           "stitch_{}".format(config.model.encoder.stitching),
+                           "{}_sessions".format(num_train_sessions)
+                          )
     
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
@@ -151,191 +156,192 @@ if args.train:
 
 #########################
 
-print('Start model evaluation.')
-print('=======================')
-
-mask_name = f"mask_{args.mask_mode}"
-if args.model_name == "NDT2":
-    model_name = "STPatch"
-else:
-    model_name = args.model_name
+if args.eval:
+    print('Start model evaluation.')
+    print('=======================')
     
-n_time_steps = 100
-
-co_smooth = True
-forward_pred = True
-inter_region = True
-intra_region = True
-choice_decoding = True
-continuous_decoding = True
-
-print(mask_name)
-
-if args.prompting:
-    model_config = f"src/configs/{model_acroynm}_stitching_prompting_eval.yaml"
-else:
-    model_config = f"src/configs/{model_acroynm}_stitching_eval.yaml"
-
-
-configs = {
-    'model_config': model_config,
-    'model_path': f'{base_path}/results/train/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/model_best.pt',
-    'trainer_config': f'src/configs/trainer_{model_acroynm}.yaml',
-    'dataset_path': None, 
-    'test_size': 0.2,
-    'seed': 42,
-    'mask_name': mask_name,
-    'eid': eid,
-    'stitching': True,
-    'num_sessions': 1 
-}  
-
-
-# load your model and dataloader
-model, accelerator, dataset, dataloader = load_model_data_local(**configs)
-
-# co-smoothing
-if co_smooth:
-    print('Start co-smoothing:')
-    co_smoothing_configs = {
-        'subtract': 'task',
-        'onset_alignment': [40],
-        'method_name': mask_name, 
-        'save_path': f'{base_path}/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/co_smooth',
-        'mode': 'per_neuron',
-        'n_time_steps': n_time_steps,    
-        'is_aligned': True,
-        'target_regions': None,
-        'n_jobs': 8
-    }
-
-    results = co_smoothing_eval(model, 
-                    accelerator, 
-                    dataloader, 
-                    dataset, 
-                    **co_smoothing_configs)
-    print(results)
-    wandb.log(results)
-
-
-# forward prediction
-if forward_pred:
-    print('Start forward prediction:')
-    results = co_smoothing_configs = {
-        'subtract': 'task',
-        'onset_alignment': [],
-        'method_name': mask_name, 
-        'save_path': f'{base_path}/results/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/forward_pred',
-        'mode': 'forward_pred',
-        'n_time_steps': n_time_steps,    
-        'held_out_list': list(range(90, 100)), # NLB uses 200 ms for fp
-        'is_aligned': True,
-        'target_regions': None,
-        'n_jobs': 8
-    }
-
-    results = co_smoothing_eval(model, 
-                    accelerator, 
-                    dataloader, 
-                    dataset, 
-                    **co_smoothing_configs)
-    print(results)
-    wandb.log(results)
+    mask_name = f"mask_{args.mask_mode}"
+    if args.model_name == "NDT2":
+        model_name = "STPatch"
+    else:
+        model_name = args.model_name
+        
+    n_time_steps = 100
     
-
-# inter-region
-if inter_region:
-    print('Start inter-region:')
-    co_smoothing_configs = {
-        'subtract': 'task',
-        'onset_alignment': [40],
-        'method_name': mask_name,
-        'save_path': f'{base_path}/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/inter_region',
-        'mode': 'inter_region',
-        'n_time_steps': n_time_steps,    
-        'held_out_list': None,
-        'is_aligned': True,
-        'target_regions': ['all'],
-        'n_jobs': 8
-    }
-
-    results = co_smoothing_eval(model, 
-                    accelerator, 
-                    dataloader, 
-                    dataset, 
-                    **co_smoothing_configs)
-    print(results)
-    wandb.log(results)
-
-
-# intra-region
-if intra_region:
-    print('Start intra-region:')
-    co_smoothing_configs = {
-        'subtract': 'task',
-        'onset_alignment': [40],
-        'method_name': mask_name, 
-        'save_path': f'{base_path}/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/intra_region',
-        'mode': 'intra_region',
-        'n_time_steps': n_time_steps,    
-        'held_out_list': None,
-        'is_aligned': True,
-        'target_regions': ['all'],
-        'n_jobs': 8
-    }
-
-    results = co_smoothing_eval(model, 
-                    accelerator, 
-                    dataloader, 
-                    dataset, 
-                    **co_smoothing_configs)
-    print(results)
-    wandb.log(results)
-
-
-if choice_decoding:
-    print('Start choice_decoding:')
+    co_smooth = False
+    forward_pred = False
+    inter_region = False
+    intra_region = False
+    choice_decoding = True
+    continuous_decoding = True
+    
+    print(mask_name)
+    
+    if args.prompting:
+        model_config = f"src/configs/{model_acroynm}_stitching_prompting_eval.yaml"
+    else:
+        model_config = f"src/configs/{model_acroynm}_stitching_eval.yaml"
+    
+    
     configs = {
         'model_config': model_config,
         'model_path': f'{base_path}/results/train/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/model_best.pt',
-        'trainer_config': f'src/configs/trainer_sl_choice_{model_acroynm}.yaml',
-        'dataset_path': None,
-        'save_path': f'{base_path}/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True//choice_decoding',
-        'test_size': 0.2,
-        'seed': 42,
-        'mask_name': mask_name,
-        'metric': 'acc',
-        'from_scratch': False,
-        'freeze_encoder': True,
-        'mask_ratio': args.mask_ratio,
-        'eid': eid,
-        'num_sessions': 1 
-    }  
-    results = behavior_decoding(**configs)
-    print(results)
-    wandb.log(results)
-
-
-if continuous_decoding:
-    print('Start continuous_decoding:')
-    configs = {
-        'model_config': model_config,
-        'model_path': f'{base_path}/results/train/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/model_best.pt',
-        'trainer_config': f'src/configs/trainer_sl_continuous_{model_acroynm}.yaml',
+        'trainer_config': f'src/configs/trainer_{model_acroynm}.yaml',
         'dataset_path': None, 
-        'save_path': f'{base_path}/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/continuous_decoding',
         'test_size': 0.2,
         'seed': 42,
         'mask_name': mask_name,
-        'metric': 'rsquared',
-        'from_scratch': False,
-        'freeze_encoder': True,
-        'mask_ratio': args.mask_ratio,
         'eid': eid,
+        'stitching': True,
         'num_sessions': 1 
     }  
-    results = behavior_decoding(**configs)
-    print(results)
-    wandb.log(results)
+    
+    
+    # load your model and dataloader
+    model, accelerator, dataset, dataloader = load_model_data_local(**configs)
+    
+    # co-smoothing
+    if co_smooth:
+        print('Start co-smoothing:')
+        co_smoothing_configs = {
+            'subtract': 'task',
+            'onset_alignment': [40],
+            'method_name': mask_name, 
+            'save_path': f'{base_path}/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/co_smooth',
+            'mode': 'per_neuron',
+            'n_time_steps': n_time_steps,    
+            'is_aligned': True,
+            'target_regions': None,
+            'n_jobs': 8
+        }
+    
+        results = co_smoothing_eval(model, 
+                        accelerator, 
+                        dataloader, 
+                        dataset, 
+                        **co_smoothing_configs)
+        print(results)
+        wandb.log(results)
+    
+    
+    # forward prediction
+    if forward_pred:
+        print('Start forward prediction:')
+        results = co_smoothing_configs = {
+            'subtract': 'task',
+            'onset_alignment': [],
+            'method_name': mask_name, 
+            'save_path': f'{base_path}/results/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/forward_pred',
+            'mode': 'forward_pred',
+            'n_time_steps': n_time_steps,    
+            'held_out_list': list(range(90, 100)), # NLB uses 200 ms for fp
+            'is_aligned': True,
+            'target_regions': None,
+            'n_jobs': 8
+        }
+    
+        results = co_smoothing_eval(model, 
+                        accelerator, 
+                        dataloader, 
+                        dataset, 
+                        **co_smoothing_configs)
+        print(results)
+        wandb.log(results)
+        
+    
+    # inter-region
+    if inter_region:
+        print('Start inter-region:')
+        co_smoothing_configs = {
+            'subtract': 'task',
+            'onset_alignment': [40],
+            'method_name': mask_name,
+            'save_path': f'{base_path}/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/inter_region',
+            'mode': 'inter_region',
+            'n_time_steps': n_time_steps,    
+            'held_out_list': None,
+            'is_aligned': True,
+            'target_regions': ['all'],
+            'n_jobs': 8
+        }
+    
+        results = co_smoothing_eval(model, 
+                        accelerator, 
+                        dataloader, 
+                        dataset, 
+                        **co_smoothing_configs)
+        print(results)
+        wandb.log(results)
+    
+    
+    # intra-region
+    if intra_region:
+        print('Start intra-region:')
+        co_smoothing_configs = {
+            'subtract': 'task',
+            'onset_alignment': [40],
+            'method_name': mask_name, 
+            'save_path': f'{base_path}/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/intra_region',
+            'mode': 'intra_region',
+            'n_time_steps': n_time_steps,    
+            'held_out_list': None,
+            'is_aligned': True,
+            'target_regions': ['all'],
+            'n_jobs': 8
+        }
+    
+        results = co_smoothing_eval(model, 
+                        accelerator, 
+                        dataloader, 
+                        dataset, 
+                        **co_smoothing_configs)
+        print(results)
+        wandb.log(results)
+    
+    
+    if choice_decoding:
+        print('Start choice_decoding:')
+        configs = {
+            'model_config': model_config,
+            'model_path': f'{base_path}/results/train/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/model_best.pt',
+            'trainer_config': f'src/configs/trainer_sl_choice_{model_acroynm}.yaml',
+            'dataset_path': None,
+            'save_path': f'{base_path}/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True//choice_decoding',
+            'test_size': 0.2,
+            'seed': 42,
+            'mask_name': mask_name,
+            'metric': 'acc',
+            'from_scratch': False,
+            'freeze_encoder': True,
+            'mask_ratio': args.mask_ratio,
+            'eid': eid,
+            'num_sessions': 1 
+        }  
+        results = behavior_decoding(**configs)
+        print(results)
+        wandb.log(results)
+    
+    
+    if continuous_decoding:
+        print('Start continuous_decoding:')
+        configs = {
+            'model_config': model_config,
+            'model_path': f'{base_path}/results/train/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/model_best.pt',
+            'trainer_config': f'src/configs/trainer_sl_continuous_{model_acroynm}.yaml',
+            'dataset_path': None, 
+            'save_path': f'{base_path}/results/{eid}/eval/multi_sessions/model_NDT1/method_ssl/{mask_name}/stitch_True/continuous_decoding',
+            'test_size': 0.2,
+            'seed': 42,
+            'mask_name': mask_name,
+            'metric': 'rsquared',
+            'from_scratch': False,
+            'freeze_encoder': True,
+            'mask_ratio': args.mask_ratio,
+            'eid': eid,
+            'num_sessions': 1 
+        }  
+        results = behavior_decoding(**configs)
+        print(results)
+        wandb.log(results)
     
 
