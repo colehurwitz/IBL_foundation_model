557167c761e70a5d5e9c1a13435cfe96a53d6277
PPWangyc <ppwang@stanford.edu>
2024-05-15 19:35:29 -0400

    sbatch config

--- script/hpc/train_sessions.sh
+++ script/hpc/train_sessions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name=multi-session
-#SBATCH --output=multi-session.out
+#SBATCH --output=multi-session-%j.out
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:rtx8000:1
 #SBATCH -t 2-12:00:00 
 #SBATCH --mem=64g
 
