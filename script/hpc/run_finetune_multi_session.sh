24544765a66e7a426f1a2b79fe94f31e3b3a8cd3
PPWangyc <ppwang@stanford.edu>
2024-05-17 00:42:36 -0400

    fix errs

--- script/hpc/run_finetune_multi_session.sh
+++ script/hpc/run_finetune_multi_session.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 conda activate ibl-fm
-cd ../../
 
 MODEL_NAME=${1}
 MASK_MODE=${2}
@@ -9,9 +8,8 @@ NUM_TRAIN_SESSIONS=${3}
 
 while IFS= read -r line
 do
-    echo "Text read from file: $line"
-    sbatch fine_tune_multi_session.sh $MODEL_NAME $MASK_MODE $NUM_TRAIN_SESSIONS $line
-done < "data/test_re_eids.txt"
+    echo "Evaluate on test eid: $line"
+    sbatch finetune_eval_multi_session.sh $MODEL_NAME $MASK_MODE $NUM_TRAIN_SESSIONS $line
+done < "../../data/test_re_eids.txt"
 
-cd script/hpc
 conda deactivate
