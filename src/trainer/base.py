adb7cc9e72560ba9a1795d840a42cf3060064838
Charan Santhirasegaran <csanthirasegaran@dt-login03.delta.ncsa.illinois.edu>
2024-10-29 03:18:06 -0500

    updates mostly to masking

--- src/trainer/base.py
+++ src/trainer/base.py
@@ -5,10 +5,8 @@ import os
 from utils.utils import move_batch_to_device, metrics_list, plot_gt_pred, plot_neurons_r2
 from tqdm import tqdm
 import random
+from pytorch_memlab import profile, MemReporter
 
-from torch.profiler import profile, record_function, ProfilerActivity
-import gc
-import torch.utils.bottleneck as bottleneck
 
 class Trainer():
     def __init__(
@@ -21,6 +19,7 @@ class Trainer():
     ):
         # get all the arguments
         self.model = model
+        self.reporter = MemReporter(self.model) #NEW
         self.train_dataloader = train_dataloader
         self.eval_dataloader = eval_dataloader
         self.optimizer = optimizer
@@ -56,58 +55,91 @@ class Trainer():
     def train(self):
         best_eval_loss = torch.tensor(float('inf'))
         best_eval_trial_avg_metric = -torch.tensor(float('inf'))
+        # train loop
+        for epoch in range(self.config.training.num_epochs):
+            train_epoch_results = self.train_epoch(epoch)
+            eval_epoch_results = self.eval_epoch()
+            print(f"epoch: {epoch} train loss: {train_epoch_results['train_loss'] }")
 
-        # Initialize the profiler
-        with profile(
-            activities=[ProfilerActivity.CUDA],
-            record_shapes=True,
-            with_flops=True,
-            profile_memory=True,
-        ) as prof:
-            for epoch in range(self.config.training.num_epochs):
-                train_epoch_results = self.train_epoch(epoch)
-                eval_epoch_results = self.eval_epoch()
-                print(f"epoch: {epoch} train loss: {train_epoch_results['train_loss'] }")
+            if eval_epoch_results:
+                if eval_epoch_results[f'eval_trial_avg_{self.metric}'] > best_eval_trial_avg_metric:
+                # if eval_epoch_results[f'eval_loss'] < best_eval_loss:
+                    best_eval_loss = eval_epoch_results[f'eval_loss']
+                    best_eval_trial_avg_metric = eval_epoch_results[f'eval_trial_avg_{self.metric}']
+                    print(f"epoch: {epoch} best eval loss: {best_eval_loss}")
+                    print(f"epoch: {epoch} best eval trial avg {self.metric}: {best_eval_trial_avg_metric}")
+                    # save model
+                    self.save_model(name="best", epoch=epoch)
+                    if self.config.method.model_kwargs.method_name == 'ssl':
+                        gt_pred_fig = self.plot_epoch(
+                            gt=eval_epoch_results['eval_gt'][0], 
+                            preds=eval_epoch_results['eval_preds'][0], epoch=epoch,
+                            active_neurons=self.session_active_neurons[0][:5]
+                        )
 
-                if eval_epoch_results:
-                    if eval_epoch_results[f'eval_trial_avg_{self.metric}'] > best_eval_trial_avg_metric:
-                        best_eval_loss = eval_epoch_results[f'eval_loss']
-                        best_eval_trial_avg_metric = eval_epoch_results[f'eval_trial_avg_{self.metric}']
-                        print(f"epoch: {epoch} best eval loss: {best_eval_loss}")
-                        print(f"epoch: {epoch} best eval trial avg {self.metric}: {best_eval_trial_avg_metric}")
-                        self.save_model(name="best", epoch=epoch)
+                        if self.config.wandb.use:
+                            wandb.log({"best_epoch": epoch,
+                                    "best_gt_pred_fig": wandb.Image(gt_pred_fig['plot_gt_pred']),
+                                    "best_r2_fig": wandb.Image(gt_pred_fig['plot_r2'])})
 
-        print('Done profiling')
-        # After profiling, filter and print the least memory-intensive operations
-        key_averages = prof.key_averages()
-        
-        # Sort by memory usage
-        sorted_averages = sorted(key_averages, key=lambda x: x.cuda_memory_usage)
+                        else:
+                            gt_pred_fig['plot_gt_pred'].savefig(
+                                os.path.join(self.log_dir, f"best_gt_pred_fig_{epoch}.png")
+                            )
+                            gt_pred_fig['plot_r2'].savefig(
+                                os.path.join(self.log_dir, f"best_r2_fig_{epoch}.png")
+                            )
 
-        # Print the least memory-intensive operations
-        print("Least memory-intensive operations:")
-        for avg in sorted_averages[-10:]:  # Adjust the number as needed
-            print(f"{avg.key}: {avg.cuda_memory_usage} bytes")
+                print(f"epoch: {epoch} eval loss: {eval_epoch_results['eval_loss']} {self.metric}: {eval_epoch_results[f'eval_trial_avg_{self.metric}']}")
 
-        # Optionally, export profiling results to a file
-        prof.export_chrome_trace(f"{self.log_dir}/train_trace_final.json")
+            # save model by epoch
+            if epoch % self.config.training.save_every == 0:
+                self.save_model(name="epoch", epoch=epoch)
 
-        # Save the last model
+            # plot epoch
+            if epoch % self.config.training.save_plot_every_n_epochs == 0:
+                if self.config.method.model_kwargs.method_name == 'ssl':
+
+                    gt_pred_fig = self.plot_epoch(
+                        gt=eval_epoch_results['eval_gt'][0], 
+                        preds=eval_epoch_results['eval_preds'][0], 
+                        epoch=epoch,
+                        active_neurons=self.session_active_neurons[0][:5]
+                    )
+                    if self.config.wandb.use:
+                        wandb.log({
+                            "gt_pred_fig": wandb.Image(gt_pred_fig['plot_gt_pred']),
+                            "r2_fig": wandb.Image(gt_pred_fig['plot_r2'])
+                        })
+                    else:
+                        gt_pred_fig['plot_gt_pred'].savefig(
+                            os.path.join(self.log_dir, f"gt_pred_fig_{epoch}.png")
+                        )
+                        gt_pred_fig['plot_r2'].savefig(
+                            os.path.join(self.log_dir, f"r2_fig_{epoch}.png")
+                        )
+
+            # wandb log
+            if self.config.wandb.use:
+                wandb.log({
+                    "train_loss": train_epoch_results['train_loss'],
+                    "eval_loss": eval_epoch_results['eval_loss'],
+                    f"eval_trial_avg_{self.metric}": eval_epoch_results[f'eval_trial_avg_{self.metric}']
+                })
+                
+        # save last model
         self.save_model(name="last", epoch=epoch)
-
+        
         if self.config.wandb.use:
             wandb.log({"best_eval_loss": best_eval_loss,
                        f"best_eval_trial_avg_{self.metric}": best_eval_trial_avg_metric})
-        
+
+    @profile                #NEW
     def train_epoch(self, epoch):
         train_loss = 0.
         train_examples = 0
         self.model.train()
-        # skip = False    #NEW
         for batch in tqdm(self.train_dataloader):
-            # if skip == True:    #NEW
-            #     continue    #NEW
-            # skip=True   #NEW
             if self.masking_mode in ["combined", "all"]:
                 masking_mode = random.sample(self.masking_schemes, 1)[0]
                 if masking_mode == 'temporal':
@@ -118,10 +150,8 @@ class Trainer():
                     self.model.encoder.masker.ratio = self.masking_ratio
             else:
                 masking_mode = self.masking_mode
-            # with record_function("forward_pass"):   #NEW
             outputs = self._forward_model_outputs(batch, masking_mode)
             loss = outputs.loss
-            # with record_function("backward_pass"):  #NEW
             loss.backward()
             self.optimizer.step()
             self.lr_scheduler.step()
